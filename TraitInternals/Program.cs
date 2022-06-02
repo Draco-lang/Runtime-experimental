814e6deb428f489d5e305186324a0ca74a51aa5f
WhiteBlackGoose <wbg@angouri.org>
2022-06-02 11:43:05 +0300

    Minor

--- TraitInternals/Program.cs
+++ TraitInternals/Program.cs
@@ -2,10 +2,7 @@
 
 quack(56)
 
-quack<T> (a : T) =
-where T : CanQuack<T>
-=
-a.Quack()
+quack<T> (a : T) = where T : CanQuack<T> = a.Quack()
 
 trait CanQuack<T>
 {
@@ -19,20 +16,45 @@ impl CanQuack<int> for int
 
 */
 
+// quack(56)
 quack(56);
 
+/*
+
+quack<T> (a : T) = where T : CanQuack<T> = a.Quack()
+
+*/
 [TraitConstraint(toConstrain: "T", traitName: "CanQuack", traitTypeArgs: new [] { "T" })]
 static void quack<T>(T a)
 {
-    var methodToInvoke = Internals.GetMethod<T, CanQuack<T>, T, Unit>(0);
+    var methodToInvoke = Internals.GetMethod<T, CanQuack<T>, T, Unit>(methodId: 0);
     methodToInvoke(a);
 }
+
+/*
+
+trait CanQuack<T>
+{
+    Quack : T -> ()
+}
+
+
+*/
 
 interface CanQuack<T>
 {
     // methodId : 0
     Unit Quack(T a);
 }
+
+/*
+
+impl CanQuack<int> for int
+{
+    Quack a = ()
+}
+
+*/
 
 public static class CanQuack_int_impl_for_Int
 {
